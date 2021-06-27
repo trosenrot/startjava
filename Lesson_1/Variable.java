a95baabd8b94a0f9a536ea79d7de675b292b3903
Oksana Terekhova <trose@yandex.ru>
2021-06-27 22:04:53 +0300

    implement HW 1_6

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main (String[] args) {
+        byte cores = 4;
+        short ram = 32;
+        int system_type = 64;
+        long processor_number = 3770;
+        float base_frequency = 3.50f;
+        double max_frequency = 3.90;
+        char operating_system = 'W';
+        boolean activate = true;
+
+        System.out.println (cores);
+        System.out.println (ram);
+        System.out.println (system_type);
+        System.out.println (processor_number);
+        System.out.println (base_frequency);
+        System.out.println (max_frequency);
+        System.out.println (operating_system);
+        System.out.println (activate);
+    }
+}
