fbbc95c1f2dcd49617b4aa0b9f6ca4b498a79912
Oksana Terekhova <trose@yandex.ru>
2021-06-29 21:34:08 +0300

    corrected HW 1_6

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -3,19 +3,19 @@ public class Variable {
         byte cores = 4;
         short ram = 32;
         int systemType = 64;
-        long processorNumber = 3770;
+        long cpuNumber = 3770;
         float baseFrequency = 3.50f;
         double maxFrequency = 3.90;
         char operatingSystem = 'W';
-        boolean activate = true;
+        boolean activated = true;
 
         System.out.println(cores);
         System.out.println(ram);
         System.out.println(systemType);
-        System.out.println(processorNumber);
+        System.out.println(cpuNumber);
         System.out.println(baseFrequency);
         System.out.println(maxFrequency);
         System.out.println(operatingSystem);
-        System.out.println(activate);
+        System.out.println(activated);
     }
 }
