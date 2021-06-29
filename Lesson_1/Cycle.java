313c648b51af8e4d0d584b56ea11947db9ef4f89
Oksana Terekhova <trose@yandex.ru>
2021-06-29 22:02:59 +0300

    implement HW 1_8

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,25 @@
+public class Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 21; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int i = 6;
+        while (i >= -6) {
+            System.out.print(i + " ");
+            i = i - 2;
+        }
+        System.out.println();
+
+        i = 10;
+        int sum = 0;
+        do {
+            if (i % 2 == 1) {
+                sum = sum + i;
+            }
+            i++;
+        } while (i <= 20);
+        System.out.println(sum);
+    }
+}
