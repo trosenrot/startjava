6022d8b506b32cf2c2bd78456edab0dd802d6265
Oksana Terekhova <trose@yandex.ru>
2021-06-29 21:39:12 +0300

    corrected HW 1_7

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,16 +1,12 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-
         short age = 28;
-        char sex = 'W';
-        boolean isSexMale = sex == 'M';
-        float height = 1.70f;
-        char firstCharName = 'O';
-
         if (age > 20) {
             System.out.println("Возраст больше 20");
         }
 
+        char sex = 'W';
+        boolean isSexMale = sex == 'M';
         if (isSexMale) {
             System.out.println("Пол мужской");
         }
@@ -19,12 +15,14 @@ public class ConditionalStatement {
             System.out.println("Пол не мужской");
         }
 
+        float height = 1.70f;
         if (height < 1.80) {
             System.out.println("Рост больше 1,80");
         } else {
             System.out.println("Рост меньше 1,80");
         }
 
+        char firstCharName = 'O';
         if (firstCharName == 'M') {
             System.out.println("Первая буква имени М");
         } else if (firstCharName == 'I') {
