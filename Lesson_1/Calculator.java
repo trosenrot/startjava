6114887ed1a557082f87c755486fd475a7e82579
Oksana Terekhova <trose@yandex.ru>
2021-07-01 11:36:51 +0300

    corrected HW 1 (Calculator)

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -3,29 +3,28 @@ public class Calculator {
         double x = 5;
         double y = 7;
         double result = 0;
-        for (char c = 33; c < 127; c++){
-            if (c == '+') {
-                result = x + y;
-                System.out.println(x + " + " + y + " = " + result);
-            } else if (c == '-') {
-                result = x - y;
-                System.out.println(x + " - " + y + " = " + result);
-            } else if (c == '/') {
-                result = x / y;
-                System.out.println(x + " / " + y + " = " + result);
-            } else if (c == '*') {
-                result = x * y;
-                System.out.println(x + " * " + y + " = " + result);
-            } else if (c == '%') {
-                result = x % y;
-                System.out.println(x + " % " + y + " = " + result);
-            } else if (c == '^') {
-                result = 1;
-                for (int j = 0; j < y; j++) {
-                    result = result * x;
-                }
-                System.out.println(x + " ^ " + y + " = " + result);
+        char c = '-';
+        if (c == '+') {
+            result = x + y;
+            System.out.println(x + " + " + y + " = " + result);
+        } else if (c == '-') {
+            result = x - y;
+            System.out.println(x + " - " + y + " = " + result);
+        } else if (c == '/') {
+            result = x / y;
+            System.out.println(x + " / " + y + " = " + result);
+        } else if (c == '*') {
+            result = x * y;
+            System.out.println(x + " * " + y + " = " + result);
+        } else if (c == '%') {
+            result = x % y;
+            System.out.println(x + " % " + y + " = " + result);
+        } else if (c == '^') {
+            result = 1;
+            for (int j = 0; j < y; j++) {
+                result *= x;
             }
+            System.out.println(x + " ^ " + y + " = " + result);
         }
     }
 }
